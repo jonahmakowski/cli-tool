3244a3a4fd638fefaf14925371b0a916fe9c624f
jonahmakowski <jonah@makowski.ca>
2026-08-02 16:43:06 -0300

    refactor: restructure config loading with match expressions

--- src/config.rs
+++ src/config.rs
@@ -32,22 +32,21 @@ pub struct TvConfig {
 }
 
 pub fn load_config() -> Config {
-    if let Some(proj_dirs) = ProjectDirs::from("com", "jonahmakowski", "cli-tool") {
-        let config_path = proj_dirs.config_dir().join("config.yaml");
+    match ProjectDirs::from("com", "jonahmakowski", "cli-tool") {
+        Some(proj_dirs) => {
+            let config_path = proj_dirs.config_dir().join("config.yaml");
 
-        match fs::read_to_string(&config_path) {
-            Ok(config_text) => match yaml_serde::from_str::<Config>(&config_text) {
-                Ok(config) => {
-                    return config;
-                }
-                Err(_) => panic!("Config file has syntax error"),
-            },
-            Err(_) => panic!(
-                "Could not read config file. Maybe it doesn't exist? Create it at \"{}\"",
-                config_path.to_string_lossy()
-            ),
+            match fs::read_to_string(&config_path) {
+                Ok(config_text) => match yaml_serde::from_str::<Config>(&config_text) {
+                    Ok(config) => config,
+                    Err(_) => panic!("Config file has syntax error"),
+                },
+                Err(_) => panic!(
+                    "Could not read config file. Maybe it doesn't exist? Create it at \"{}\"",
+                    config_path.to_string_lossy()
+                ),
+            }
         }
+        None => panic!("Couldn't find a valid location for config file"),
     }
-
-    panic!("Couldn't find a valid location for config file");
 }
